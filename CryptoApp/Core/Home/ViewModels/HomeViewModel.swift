32ea7c4a5093acd531f7065da6fc5412d1337db3
Marina Zhukova <marinazhukova28@gmail.com>
2024-12-16 21:06:23 +0200

    add column

---
+++ CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewModel.swift
+//  CryptoApp
+//
+//  Created by Marina Zhukova on 13.12.2024.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+        }
+    }
+}
